e7e933e9a76cb1ea9e7244026b2eebf2e24e3821
gkallingal <george.kallingal@am.sony.com>
2016-03-11 11:49:37 -0800

    Add fill method

--- src/module2/MyPApplet.java
+++ src/module2/MyPApplet.java
@@ -17,6 +17,7 @@ public class MyPApplet extends PApplet {
 	public void draw(){
 		backgroundImg.resize(width, height);
 		image(backgroundImg, 0, 0);
+		fill(255, 200, 0);
 		ellipse(width/4, height/5, width/4, height/4);
 		
 	}
