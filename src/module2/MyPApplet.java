df1f7e7f0478e13c5bec54bdcacd5c5627e45512
gkallingal <george.kallingal@am.sony.com>
2016-03-11 10:48:46 -0800

    Commit of OOP_Java Coursera files

---
+++ src/module2/MyPApplet.java
@@ -0,0 +1,23 @@
+package module2;
+
+import processing.core.*;
+
+public class MyPApplet extends PApplet {
+	
+	private String image = "palmTrees.jpg";
+	
+	private PImage backgroundImg;
+	
+	public void setup(){
+		
+		size(200, 200);
+		backgroundImg = loadImage(image, "jpg");
+	}
+
+	public void draw(){
+		backgroundImg.resize(width, height);
+		image(backgroundImg, 0, 0);
+		ellipse(width/4, height/5, width/4, height/4);
+		
+	}
+}
